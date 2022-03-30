e2e5c54e372758a532ab9a54a248646f48263133
mjaichand <mjaichandms@gmail.com>
2022-03-30 01:27:20 -0500

    bonus part completed

--- routes/computation.js
+++ routes/computation.js
@@ -1,18 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const x=search_params.get("x") || getRandomArbitrary(0, 60);
-var y1 = Math.log2(x);
-var y2 = Math.cosh(x);
-var y3 = Math.floor(x);
-function getRandomArbitrary(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  times++;
-  res.send(`Math.log2(${x}) is ${y1}\n`);
-  res.send(`Math.cosh(${x}) is ${y2}\n`);
-  res.send(`Math.floor(${x}) is ${y3}\n`);
+  const x= req.query.x || getRandomArbitrary(0, 60);
+  var y1 = Math.log2(x);
+  var y2 = Math.cosh(x);
+  var y3 = Math.floor(x);
+  function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  res.send(`Math.log2(${x}) is ${y1} ; Math.cosh(${x}) is ${y2} ; Math.floor(${x}) is ${y3}\n`);
 });
 
 module.exports = router;
